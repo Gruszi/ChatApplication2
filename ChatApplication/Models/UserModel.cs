d274fac994aa14bfa2640702c58733a534f1a899
Mirek <Mirek@DESKTOP-BPSQ5V2>
2022-06-24 20:32:04 +0200

    Dodaj pliki projektów.

---
+++ ChatApplication/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿public class UserModel
+{
+    public int userid { get; set; }
+    [Required(ErrorMessage = "Email id Is required")]
+    public string email { get; set; }
+    [Required(ErrorMessage = "Mobile Number Is required")]
+    public string mobile { get; set; }
+    [Required(ErrorMessage = "Password Is required")]
+    public string password { get; set; }
+    [Required(ErrorMessage = "Confirm Password Is required")]
+    [Compare("password", ErrorMessage = "Password and Confirmation Password must match.")]
+    public string confirmpassword { get; set; }
+    [Required(ErrorMessage = "Date Of Birth Is required")]
+    public string dob { get; set; }
+}
